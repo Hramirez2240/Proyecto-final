b134318a6b05608c6490780399812192709f0eaf
Hramirez2240 <hramirez2298@gmail.com>
2021-04-22 18:25:38 -0400

    Tablas, cedula

---
+++ Proyecto-final/Models/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Proyecto_final.Models
+{
+    public partial class Cliente
+    {
+        public int Id { get; set; }
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+        public string Licencia { get; set; }
+        public string Nacionalidad { get; set; }
+        public string Sangre { get; set; }
+        public string Ft1 { get; set; }
+        public string Ft2 { get; set; }
+        public bool? Habilitar { get; set; }
+    }
+}
